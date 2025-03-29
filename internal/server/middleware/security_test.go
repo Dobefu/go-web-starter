bd489d383c829a0cd1621ca3d4df1617ce38702b
Connor van Spronssen <info@dobefu.com>
2025-03-29 21:56:42 +0100

    test: Add a test to the security header middleware

---
+++ internal/server/middleware/security_test.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSecurityHeaders(t *testing.T) {
+	t.Parallel()
+
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+	router.Use(SecurityHeaders())
+
+	router.GET("/", func(c *gin.Context) {
+		c.Status(http.StatusOK)
+	})
+
+	w := httptest.NewRecorder()
+	req, err := http.NewRequest("GET", "/", nil)
+	assert.NoError(t, err)
+	router.ServeHTTP(w, req)
+
+	headers := w.Header()
+
+	assert.Equal(t, "DENY", headers.Get("X-Frame-Options"))
+	assert.Equal(t, "1; mode=block", headers.Get("X-XSS-Protection"))
+	assert.Equal(t, "nosniff", headers.Get("X-Content-Type-Options"))
+	assert.Equal(t, "strict-origin-when-cross-origin", headers.Get("Referrer-Policy"))
+}
