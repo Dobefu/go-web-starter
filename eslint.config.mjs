977e5b0fb8e620a97a29795616616e457a67842d
Connor van Spronssen <info@dobefu.com>
2025-03-30 13:39:21 +0200

    feat: Add ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+])
