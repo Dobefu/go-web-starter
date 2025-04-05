89bbff2837f80524b1b05b8b0379083e97b6fe10
Connor van Spronssen <info@dobefu.com>
2025-04-05 22:26:04 +0200

    refactor: Rename the `redis` import to `redisClient`, for consistency

--- internal/redis/redis.go
+++ internal/redis/redis.go
@@ -7,26 +7,26 @@ import (
 
 	"github.com/Dobefu/go-web-starter/internal/config"
 	"github.com/Dobefu/go-web-starter/internal/logger"
-	redis "github.com/redis/go-redis/v9"
+	redisClient "github.com/redis/go-redis/v9"
 )
 
 type RedisInterface interface {
 	Close() error
-	Get(ctx context.Context, key string) (*redis.StringCmd, error)
-	Set(ctx context.Context, key string, value any, expiration time.Duration) (*redis.StatusCmd, error)
-	GetRange(ctx context.Context, key string, start, end int64) (*redis.StringCmd, error)
-	SetRange(ctx context.Context, key string, offset int64, value string) (*redis.IntCmd, error)
+	Get(ctx context.Context, key string) (*redisClient.StringCmd, error)
+	Set(ctx context.Context, key string, value any, expiration time.Duration) (*redisClient.StatusCmd, error)
+	GetRange(ctx context.Context, key string, start, end int64) (*redisClient.StringCmd, error)
+	SetRange(ctx context.Context, key string, offset int64, value string) (*redisClient.IntCmd, error)
 }
 
 type Redis struct {
-	db     *redis.Client
+	db     *redisClient.Client
 	logger *logger.Logger
 }
 
 var errNotInitialized error = fmt.Errorf("redis not initialized")
 
 var New = func(cfg config.Redis, log *logger.Logger) (*Redis, error) {
-	db := redis.NewClient(&redis.Options{
+	db := redisClient.NewClient(&redisClient.Options{
 		Addr:     fmt.Sprintf("%s:%d", cfg.Host, cfg.Port),
 		Password: cfg.Password,
 		DB:       cfg.DB,
@@ -46,7 +46,7 @@ func (d *Redis) Close() error {
 	return d.db.Close()
 }
 
-func (d *Redis) Get(ctx context.Context, key string) (*redis.StringCmd, error) {
+func (d *Redis) Get(ctx context.Context, key string) (*redisClient.StringCmd, error) {
 	if d.db == nil {
 		return nil, errNotInitialized
 	}
@@ -54,7 +54,7 @@ func (d *Redis) Get(ctx context.Context, key string) (*redis.StringCmd, error) {
 	return d.db.Get(ctx, key), nil
 }
 
-func (d *Redis) Set(ctx context.Context, key string, value any, expiration time.Duration) (*redis.StatusCmd, error) {
+func (d *Redis) Set(ctx context.Context, key string, value any, expiration time.Duration) (*redisClient.StatusCmd, error) {
 	if d.db == nil {
 		return nil, errNotInitialized
 	}
@@ -62,7 +62,7 @@ func (d *Redis) Set(ctx context.Context, key string, value any, expiration time.
 	return d.db.Set(ctx, key, value, expiration), nil
 }
 
-func (d *Redis) GetRange(ctx context.Context, key string, start, end int64) (*redis.StringCmd, error) {
+func (d *Redis) GetRange(ctx context.Context, key string, start, end int64) (*redisClient.StringCmd, error) {
 	if d.db == nil {
 		return nil, errNotInitialized
 	}
@@ -70,7 +70,7 @@ func (d *Redis) GetRange(ctx context.Context, key string, start, end int64) (*re
 	return d.db.GetRange(ctx, key, start, end), nil
 }
 
-func (d *Redis) SetRange(ctx context.Context, key string, offset int64, value string) (*redis.IntCmd, error) {
+func (d *Redis) SetRange(ctx context.Context, key string, offset int64, value string) (*redisClient.IntCmd, error) {
 	if d.db == nil {
 		return nil, errNotInitialized
 	}
