b94bce339b94dea49660a0311bb88ea4016ca601
Connor van Spronssen <info@dobefu.com>
2025-04-06 12:24:09 +0200

    feat: Add a template function to pass a dict of arbitrary data

---
+++ internal/server/template_functions.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"html/template"
+)
+
+func TemplateFuncMap() template.FuncMap {
+	return template.FuncMap{
+		"dict": func(values ...interface{}) (dict map[string]interface{}) {
+			// If the length of the map is not an even number, it is malformed.
+			if (len(values) % 2) != 0 {
+				return nil
+			}
+
+			dict = make(map[string]interface{}, (len(values) / 2))
+
+			for i := 0; i < len(values); i += 2 {
+				key, ok := values[i].(string)
+
+				if !ok {
+					continue
+				}
+
+				dict[key] = values[i+1]
+			}
+
+			return dict
+		},
+	}
+}
