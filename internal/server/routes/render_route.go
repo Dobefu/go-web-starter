01775b20f50dbd5efa65997b5098cd24252fee43
Connor van Spronssen <info@dobefu.com>
2025-04-07 07:32:51 +0200

    feat: Add a copyright to the footer

--- internal/server/routes/render_route.go
+++ internal/server/routes/render_route.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
@@ -9,9 +11,11 @@ func RenderRouteHTML(c *gin.Context, routeData RouteData) {
 	data := struct {
 		RouteData
 		SiteName string
+		Year     string
 	}{
 		RouteData: routeData,
 		SiteName:  viper.GetString("site.name"),
+		Year:      time.Now().Format("2006"),
 	}
 
 	c.HTML(data.HttpStatus, data.Template, data)
