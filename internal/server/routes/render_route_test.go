63aab7d51224d9ce5e59282d2fec7f4e4852e9fe
Connor van Spronssen <info@dobefu.com>
2025-04-10 08:31:55 +0200

    test: Add tests to the RenderRoute function

---
+++ internal/server/routes/render_route_test.go
@@ -0,0 +1,76 @@
+package routes
+
+import (
+	"html/template"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Dobefu/go-web-starter/internal/templates"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRenderRouteHTML(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	w := httptest.NewRecorder()
+	c, r := gin.CreateTestContext(w)
+
+	viper.Set("site.name", "Test Site")
+
+	t.Run("Debug Mode Rendering", func(t *testing.T) {
+		gin.SetMode(gin.DebugMode)
+		defer gin.SetMode(gin.TestMode)
+
+		routeData := RouteData{
+			Template:   "pages/index",
+			HttpStatus: http.StatusOK,
+			Title:      "Test Page",
+		}
+
+		err := templates.LoadTemplates(r)
+		assert.NoError(t, err)
+
+		RenderRouteHTML(c, routeData)
+
+		assert.Equal(t, http.StatusOK, w.Code)
+	})
+
+	t.Run("Cached Template Rendering", func(t *testing.T) {
+		tmpl := template.Must(template.New("test.tmpl").Parse("Hello {{.Title}}"))
+		cache := templates.GetTemplateCache()
+		cache.Set("test.tmpl", tmpl)
+
+		routeData := RouteData{
+			Template:   "test.tmpl",
+			HttpStatus: http.StatusOK,
+			Title:      "Test Page",
+		}
+
+		w := httptest.NewRecorder()
+		c, _ := gin.CreateTestContext(w)
+		RenderRouteHTML(c, routeData)
+
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Contains(t, w.Body.String(), "Hello Test Page")
+	})
+
+	t.Run("Template Execution Error", func(t *testing.T) {
+		tmpl := template.Must(template.New("error.tmpl").Parse("{{.BogusField}}"))
+		cache := templates.GetTemplateCache()
+		cache.Set("error.tmpl", tmpl)
+
+		routeData := RouteData{
+			Template:   "error.tmpl",
+			HttpStatus: http.StatusOK,
+			Title:      "Test Page",
+		}
+
+		w := httptest.NewRecorder()
+		c, _ := gin.CreateTestContext(w)
+		RenderRouteHTML(c, routeData)
+
+		assert.NotNil(t, c.Errors.Last())
+	})
+}
