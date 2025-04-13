363ac51f1e0e7db2146929bc81954e19210c3aea
Connor van Spronssen <info@dobefu.com>
2025-04-13 18:25:37 +0200

    test: Add a test to the login page

---
+++ internal/server/routes/login_test.go
@@ -0,0 +1,30 @@
+package routes
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	server_utils "github.com/Dobefu/go-web-starter/internal/server/utils"
+	"github.com/Dobefu/go-web-starter/internal/templates"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLogin(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	router := gin.New()
+
+	router.SetFuncMap(server_utils.TemplateFuncMap())
+	err := templates.LoadTemplates(router)
+	assert.NoError(t, err)
+	router.GET("/", Login)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
