9b62c07ad86d41fb04b4e10e86cc218b4071f446
Connor van Spronssen <info@dobefu.com>
2025-04-13 12:25:55 +0200

    refactor: Simplify the bundling logic

--- internal/static/static/js/src/main.ts
+++ internal/static/static/js/src/main.ts
@@ -1,13 +1,1 @@
-interface Module {
-  init?: () => void
-}
-
-const modules = import.meta.glob<Module>('./components/**/*.ts')
-
-Object.values(modules).forEach(async (module) => {
-  const mod = await module()
-
-  if (mod && typeof mod.init === 'function') {
-    mod.init()
-  }
-})
+import './components/layout/header'
