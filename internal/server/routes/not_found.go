ffed0a30e15e924240e852875dbfce4252bb267b
Connor van Spronssen <info@dobefu.com>
2025-04-19 21:56:56 +0200

    chore: Slightly change the title and description for the "Not Found" page

--- internal/server/routes/not_found.go
+++ internal/server/routes/not_found.go
@@ -11,8 +11,8 @@ func NotFound(c *gin.Context) {
 		Template:   "pages/not-found",
 		HttpStatus: http.StatusNotFound,
 
-		Title:       "Not found",
-		Description: "The page could not be found",
+		Title:       "Page Not Found",
+		Description: "The page you are looking for does not exist",
 	}
 
 	RenderRouteHTML(c, data)
