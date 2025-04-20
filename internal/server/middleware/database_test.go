5e26f1c43af23785f929bdfbd6e13db50f9b0d94
Connor van Spronssen <info@dobefu.com>
2025-04-20 16:38:30 +0200

    fix: Make the database middleware test struct compatible with the database

--- internal/server/middleware/database_test.go
+++ internal/server/middleware/database_test.go
@@ -13,12 +13,12 @@ type MockDatabase struct {
 	database.DatabaseInterface
 }
 
-func (m *MockDatabase) Close() error                                         { return nil }
-func (m *MockDatabase) Ping() error                                          { return nil }
-func (m *MockDatabase) Query(query string, args ...any) (*sql.Rows, error)   { return nil, nil }
-func (m *MockDatabase) QueryRow(query string, args ...any) (*sql.Row, error) { return nil, nil }
-func (m *MockDatabase) Exec(query string, args ...any) (sql.Result, error)   { return nil, nil }
-func (m *MockDatabase) Begin() (*sql.Tx, error)                              { return nil, nil }
+func (m *MockDatabase) Close() error                                       { return nil }
+func (m *MockDatabase) Ping() error                                        { return nil }
+func (m *MockDatabase) Query(query string, args ...any) (*sql.Rows, error) { return nil, nil }
+func (m *MockDatabase) QueryRow(query string, args ...any) *sql.Row        { return nil }
+func (m *MockDatabase) Exec(query string, args ...any) (sql.Result, error) { return nil, nil }
+func (m *MockDatabase) Begin() (*sql.Tx, error)                            { return nil, nil }
 
 func TestDatabase(t *testing.T) {
 	gin.SetMode(gin.TestMode)
