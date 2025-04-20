aa60e64c75419b4bbef6d0518e30d6fe53c8b907
Connor van Spronssen <info@dobefu.com>
2025-04-20 22:21:01 +0200

    test: Simplify the server command tests

--- cmd/server_test.go
+++ cmd/server_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-const testPort = 4000
+const defaultTestPort = 4000
 
 type MockServer struct {
 	mock.Mock
@@ -20,19 +20,24 @@ func (m *MockServer) Start() error {
 	return args.Error(0)
 }
 
-func TestServerCmdSuccess(t *testing.T) {
+func setupMockServer(t *testing.T, returnError error) *MockServer {
 	mockSrv := new(MockServer)
-	mockSrv.On("Start").Return(nil)
+	mockSrv.On("Start").Return(returnError)
 
 	originalNew := server.DefaultNew
 	server.DefaultNew = func(port int) server.ServerInterface {
 		return mockSrv
 	}
+	t.Cleanup(func() { server.DefaultNew = originalNew })
 
-	defer func() { server.DefaultNew = originalNew }()
+	return mockSrv
+}
+
+func TestServerCmdSuccess(t *testing.T) {
+	mockSrv := setupMockServer(t, nil)
 
 	cmd := &cobra.Command{}
-	cmd.Flags().Int("port", testPort, "")
+	cmd.Flags().Int("port", defaultTestPort, "")
 
 	assert.NotPanics(t, func() {
 		ServerCmd(cmd, []string{})
@@ -42,16 +47,10 @@ func TestServerCmdSuccess(t *testing.T) {
 }
 
 func TestServerCmdError(t *testing.T) {
-	mockSrv := new(MockServer)
-	mockSrv.On("Start").Return(assert.AnError)
-
-	originalNew := server.DefaultNew
-	server.DefaultNew = func(port int) server.ServerInterface { return mockSrv }
-
-	defer func() { server.DefaultNew = originalNew }()
+	mockSrv := setupMockServer(t, assert.AnError)
 
 	cmd := &cobra.Command{}
-	cmd.Flags().Int("port", testPort, "")
+	cmd.Flags().Int("port", defaultTestPort, "")
 
 	assert.NotPanics(t, func() {
 		ServerCmd(cmd, []string{})
@@ -61,13 +60,7 @@ func TestServerCmdError(t *testing.T) {
 }
 
 func TestServerCmdInvalidPort(t *testing.T) {
-	mockSrv := new(MockServer)
-
-	originalNew := server.DefaultNew
-	server.DefaultNew = func(port int) server.ServerInterface {
-		return mockSrv
-	}
-	defer func() { server.DefaultNew = originalNew }()
+	mockSrv := setupMockServer(t, nil)
 
 	cmd := &cobra.Command{}
 
