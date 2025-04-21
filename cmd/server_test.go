6c5b32320bbc9809ab385cc2d390e2167322e546
Connor van Spronssen <info@dobefu.com>
2025-04-21 16:48:58 +0200

    test: Update the server command tests

--- cmd/server_test.go
+++ cmd/server_test.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"runtime"
 	"testing"
+	"time"
 
 	"github.com/Dobefu/go-web-starter/internal/server"
 	"github.com/spf13/cobra"
@@ -25,9 +27,11 @@ func setupMockServer(t *testing.T, returnError error) *MockServer {
 	mockSrv.On("Start").Return(returnError)
 
 	originalNew := server.DefaultNew
-	server.DefaultNew = func(port int) server.ServerInterface {
-		return mockSrv
+
+	server.DefaultNew = func(port int) (server.ServerInterface, error) {
+		return mockSrv, nil
 	}
+
 	t.Cleanup(func() { server.DefaultNew = originalNew })
 
 	return mockSrv
@@ -47,26 +51,51 @@ func TestServerCmdSuccess(t *testing.T) {
 }
 
 func TestServerCmdError(t *testing.T) {
+	originalOsExit := osExit
+	defer func() { osExit = originalOsExit }()
+
+	var isExitCalled bool
+
+	osExit = func(code int) {
+		isExitCalled = true
+		assert.Equal(t, 1, code, "Expected exit code 1")
+	}
+
 	mockSrv := setupMockServer(t, assert.AnError)
 
 	cmd := &cobra.Command{}
 	cmd.Flags().Int("port", defaultTestPort, "")
 
-	assert.NotPanics(t, func() {
-		ServerCmd(cmd, []string{})
-	})
+	ServerCmd(cmd, []string{})
 
 	mockSrv.AssertExpectations(t)
+	assert.True(t, isExitCalled, "osExit should have been called")
 }
 
 func TestServerCmdInvalidPort(t *testing.T) {
+	originalOsExit := osExit
+	defer func() { osExit = originalOsExit }()
+	var isExitCalled bool
+
+	osExit = func(code int) {
+		isExitCalled = true
+		assert.Equal(t, 1, code, "Expected exit code 1")
+		runtime.Goexit()
+	}
+
 	mockSrv := setupMockServer(t, nil)
 
 	cmd := &cobra.Command{}
 
-	assert.NotPanics(t, func() {
+	done := make(chan bool)
+
+	go func() {
 		ServerCmd(cmd, []string{})
-	})
+		close(done)
+	}()
+
+	time.Sleep(10 * time.Millisecond)
 
 	mockSrv.AssertNumberOfCalls(t, "Start", 0)
+	assert.True(t, isExitCalled, "osExit should have been called due to invalid port flag")
 }
