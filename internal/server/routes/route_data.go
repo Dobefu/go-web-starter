2d5c792ee660392faa1fd4e0004f2128662b56f5
Connor van Spronssen <info@dobefu.com>
2025-04-22 08:20:11 +0200

    feat: Add a struct for generic server error data

--- internal/server/routes/route_data.go
+++ internal/server/routes/route_data.go
@@ -1,5 +1,11 @@
 package routes
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type FormData struct {
 	Values   map[string]string
 	Errors   map[string][]string
@@ -11,6 +17,16 @@ type RouteData struct {
 	HttpStatus  int
 	Title       string
 	Description string
+	Data        map[string]any
 	FormData    FormData
 	CSRFToken   string
+}
+
+func GenericErrorData(c *gin.Context) RouteData {
+	return RouteData{
+		Template:    "pages/500",
+		HttpStatus:  http.StatusInternalServerError,
+		Title:       "Server Error",
+		Description: "Sorry, something went wrong on our end.",
+	}
 }
