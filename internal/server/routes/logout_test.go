a1fa345beb251ee77d246317949b5ec0dcddc0e9
Connor van Spronssen <info@dobefu.com>
2025-04-27 14:49:41 +0200

    test: Add a test to the logout route

---
+++ internal/server/routes/logout_test.go
@@ -0,0 +1,38 @@
+package routes
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	server_utils "github.com/Dobefu/go-web-starter/internal/server/utils"
+	"github.com/Dobefu/go-web-starter/internal/templates"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLogout(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	viper.Set("site.name", "Test Site")
+	viper.Set("site.host", "http://localhost:8080")
+
+	router := gin.New()
+
+	store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("mysession", store))
+
+	router.SetFuncMap(server_utils.TemplateFuncMap())
+	err := templates.LoadTemplates(router)
+	assert.NoError(t, err)
+	router.GET("/", Logout)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusSeeOther, w.Code)
+}
