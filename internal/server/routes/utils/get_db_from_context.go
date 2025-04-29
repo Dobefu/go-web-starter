5f536e0b1b5c06a8d7508dfa79a56e6082bb385c
Connor van Spronssen <info@dobefu.com>
2025-04-29 20:50:49 +0200

    fix: Resolve a nil pointer dereference

--- internal/server/routes/utils/get_db_from_context.go
+++ internal/server/routes/utils/get_db_from_context.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"errors"
+
 	"github.com/Dobefu/go-web-starter/internal/database"
 	"github.com/gin-gonic/gin"
 )
@@ -9,14 +11,13 @@ func GetDbFromContext(c *gin.Context) (db database.DatabaseInterface, err error)
 	dbVal, exists := c.Get("db")
 
 	if !exists {
-		return nil, err
+		return nil, errors.New("database not found in context")
 	}
 
 	db, ok := dbVal.(database.DatabaseInterface)
 
 	if !ok {
-
-		return nil, err
+		return nil, errors.New("database in context is not of type DatabaseInterface")
 	}
 
 	return db, nil
