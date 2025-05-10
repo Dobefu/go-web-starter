a4d8327bf636b619b4ffe877234750b963f8f91a
Connor van Spronssen <info@dobefu.com>
2025-05-10 17:12:01 +0200

    test: Add a basic test to the account deletion page

---
+++ internal/server/routes/account_delete_test.go
@@ -0,0 +1,35 @@
+package routes
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAccountDelete(t *testing.T) {
+	router, _, mockDB, err := setupTestRouter(true)
+	assert.NoError(t, err)
+	defer func() { _ = mockDB.Close() }()
+
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	store := cookie.NewStore([]byte("secret"))
+	c.Request, _ = http.NewRequest("GET", "/", nil)
+	sessions.Sessions("mysession", store)(c)
+
+	session := sessions.Default(c)
+	session.Set("userID", 1)
+	err = session.Save()
+	assert.NoError(t, err)
+
+	router.GET("/", AccountDelete)
+
+	router.ServeHTTP(w, c.Request)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
